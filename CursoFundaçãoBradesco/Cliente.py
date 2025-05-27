d0857cffacacc3b066fdb6d10c94e74b4de19bad
LucasCotrim2003 <lucasdasilvacotrim03@gmail.com>
2025-05-27 04:48:30 -0300

    Inserção de saque e deposito e finalização

---
+++ CursoFundaçãoBradesco/Cliente.py
@@ -0,0 +1,13 @@
+class Cliente:
+    def __init__(self, n, fone): 
+        
+        self.nome = n
+        self.telefone = fone
+
+    #método get 
+    def get_nome(self):
+        return self.get_nome
+    
+    # metodo set
+    def set_nome(self, nome):
+        self._nome = nome
