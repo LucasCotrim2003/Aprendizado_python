4e336b8809b5d6156a77c84f0c963f4e5aa18c14
LucasCotrim2003 <lucasdasilvacotrim03@gmail.com>
2025-08-08 13:35:42 -0300

    Base do Projeto

---
+++ App_Contador/main.py
@@ -0,0 +1,7 @@
+P = float(input("Insira o total positivo: "))
+N = float(input("Insira o total negativo: "))
+
+if P and N != float:
+    print(f"o Total foi: {P - N}")
+else:
+    print("Valor inserido inválido!")
